9dfb5e2020c642807096f499c11558e279b81379
DomyS <domante.stirbyte@gmail.com>
2017-06-21 10:33:04 +0300

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,21 @@
+<?php
+
+include("connect.php");
+include("functions.php");
+
+if(logged_in()) {
+	
+	?>
+	
+	<a href="changepassword.php">Change password</a>
+	
+	<a href="logout.php" style="float:right; padding: 10px; margin-right: 40px; background-color:#eee; color: #333; text-decoration: none; ">Logout</a>
+	
+	<?php
+}
+else {
+	header("location:login.php");
+	exit();
+}
+
+?>
